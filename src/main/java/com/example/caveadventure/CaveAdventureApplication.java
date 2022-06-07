a89b26c38dbbec110258934c29965a1313aa9b3d
bowlmeat <bowlmeat@qq.com>
2022-06-07 11:09:42 +0800

    init

---
+++ src/main/java/com/example/caveadventure/CaveAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.example.caveadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaveAdventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaveAdventureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/caveadventure/CaveAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caveadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaveAdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
