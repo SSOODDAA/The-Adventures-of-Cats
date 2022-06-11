c86f05d0d5190cb885fb169eb062c3f4dfc843dd
Lily-eclipse <Lily-eclipse@qq.com>
2022-06-12 00:45:10 +0800

    perf #12 登录界面布局优化

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,16 +1,27 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import UserLogin from "@/views/UserLogin";
 import MainGame from "@/views/MainGame";
+import UserRegister from "@/views/UserRegister";
 const routes = [
   {
     path: '/',
     name: 'UserLogin',
     component: UserLogin,
   },
+  // {
+  //   path: '/login',
+  //   name: 'UserLogin',
+  //   component: UserLogin,
+  // },
   {
     path: '/maingame',
     name: 'MainGame',
     component: MainGame,
+  },
+  {
+    path: '/register',
+    name: 'UserRegister',
+    component: UserRegister,
   },
 ]
 const router = createRouter({
