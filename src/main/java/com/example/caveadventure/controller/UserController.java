a4130851e6388642ad0781ea3104409e2172dbdd
bowlmeat <bowlmeat@qq.com>
2022-06-13 21:39:03 +0800

    更新个人信息Controller完善

--- src/main/java/com/example/caveadventure/controller/UserController.java
+++ src/main/java/com/example/caveadventure/controller/UserController.java
@@ -5,12 +5,7 @@ import com.example.caveadventure.service.UserService;
 import com.example.caveadventure.util.JsonResult;
 import java.io.Console;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -21,17 +16,31 @@ public class UserController extends BaseController{
     @Autowired
     private UserService userservice;
 
+    /* 注册 */
     @PostMapping
     public JsonResult<Void> reg(@RequestBody UserEntity user){
         userservice.reg(user);
         return new JsonResult<>(OK);
     }
 
+    /* 登录 */
     @GetMapping
     public JsonResult<UserEntity> login(@RequestParam String username,@RequestParam String pwd){
         UserEntity data = userservice.login(username, pwd);
 
         return new JsonResult<UserEntity>(OK, data);
     }
+
+    /* 更新 */
+    @PutMapping
+    public JsonResult<UserEntity> update(Integer userid, String info){
+        userservice.updatePassword(userid, info);
+
+        userservice.updateUsername(userid, info);
+
+        UserEntity data = userservice.findByUserid(userid);
+        return new JsonResult<UserEntity>(OK, data);
+    }
+
 
 }
