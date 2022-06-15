4324ad4019d2c6d1df6b0aba13382282f434c9cc
QuokkaD <1464021006@qq.com>
2022-06-15 15:58:14 +0800

    feat#17登陆后界面的VUE搭建

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from 'vue-router'
 import UserLogin from "@/views/UserLogin";
 import MainGame from "@/views/MainGame";
 import UserRegister from "@/views/UserRegister";
+import HomePage from "@/views/HomePage";
+import ChooseRole from "@/views/ChooseRole";
 const routes = [
   {
     path: '/',
@@ -24,6 +26,16 @@ const routes = [
     name: 'UserRegister',
     component: UserRegister,
   },
+  {
+    path: '/homepage',
+    name: 'HomePage',
+    component: HomePage,
+  },
+  {
+    path: '/chooserole',
+    name: 'ChooseRole',
+    component: ChooseRole,
+  }
 ]
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
